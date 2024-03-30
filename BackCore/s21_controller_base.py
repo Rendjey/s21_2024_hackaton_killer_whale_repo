f76ae89311e239d22f12eee1d608b1055279f3f3
mrMedov <rendjey@ya.ru>
2024-03-30 11:29:34 +0300

    commit_jeniffee_02

--- BackCore/s21_controller_base.py
+++ BackCore/s21_controller_base.py
@@ -1,7 +1,8 @@
 import random
 
 def base_controller():
-    random = random.randint(0, 9)
-    if(random == 7):
+    student = True
+    random_number = random.randint(1, 20)
+    if(random_number == 9):
         student = False
     return student
--- db_query.py
+++ db_query.py
@@ -5,19 +5,13 @@ conn = psycopg2.connect(dbname='hackdb', user='tester', password='tester', host=
 conn.autocommit = True
 cur = conn.cursor()
 
-#cur.execute('SELECT * FROM users')
-username = '1'
-sql = 'SELECT username FROM users WHERE username = %s'
-record_to_insert = str(username)
-cur.execute(sql)
-
-
+cur.execute('SELECT * FROM users')
 pr = cur.fetchall()
 print(pr)
 
 print("-------rooms-------")
 
-#cur.execute('ALTER TABLE rooms ADD floor INTEGER') #Добавление столбца
+#cur.execute('ALTER TABLE users ADD command VARCHAR(256)') #Добавление столбца
 #cur.execute('ALTER TABLE users RENAME COLUMN password TO uid;') #изменение наименования столбца
 
 cur.close()
